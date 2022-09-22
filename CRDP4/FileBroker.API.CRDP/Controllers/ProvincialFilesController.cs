041de603c3b1ef34847e8b18f6ea90dd1d065e36
DenSarJus <dsarrazi@justice.gc.ca>
2022-09-22 13:03:53 -0400

    Init project

---
+++ CRDP4/FileBroker.API.CRDP/Controllers/ProvincialFilesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileBroker.API.CRDP.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProvincialFilesController : ControllerBase
+    {
+        [HttpGet("Version")]
+        public ActionResult GetVersion()
+        {
+            return Ok("Version 1.0");
+        }
+    }
+}
